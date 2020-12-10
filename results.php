c9e8224f0dd057835925b4ca7497b4ba00aa1653
anoetz01 <anoetz01@nyit.edu>
2020-12-09 20:39:26 -0500

    Added h1 and h3

--- results.php
+++ results.php
@@ -5,13 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Trivia Quizzes!</title>
-    <link rel="stylesheet" type="text/css" href="quiz.css">
+    <title>Document</title>
 </head>
 <body>
-
-    <h1>Welcome to Trivia Quizzes!</h1>
-    <h3>Please select which quiz you would like to take</h3>
-
+    <h1>Results</h1>
 </body>
 </html>
